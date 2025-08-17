a3bae4f7ac86aff790e1fed6b311c117083732d1
solomonjedward <89007695+solomonjedward@users.noreply.github.com>
2025-08-17 15:15:20 +0530

    Create temp-humidity.ino

---
+++ temp-humidity.ino
@@ -0,0 +1,31 @@
+//This will show Temprature and Humidity in adreno console
+
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  if (!sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+}
+
+void loop() {
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  if (!isnan(t) && !isnan(h)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.print(t); Serial.print("	");
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else {
+    Serial.println("Failed to read from SHT31 sensor");
+  }
+
+  delay(1000);
+}
+
